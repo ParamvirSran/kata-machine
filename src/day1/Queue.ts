22b09bb1d2ab545d4b4a81d5a989d4fbe9e08850
Paramvir Sran <paramvirsran@gmail.com>
2024-06-11 01:54:24 -0400

    wrote code for queue data structure

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,50 @@
+class Node<T> {
+    value: T;
+    next: Node<T> | undefined;
+
+    constructor(value: T) {
+        this.value = value;
+        this.next = undefined;
+    }
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private front: Node<T> | undefined;
+    private back: Node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.front = undefined;
+        this.back = undefined;
     }
 
     enqueue(item: T): void {
-
-}
+        const newNode = new Node(item);
+        if (this.length == 0) {
+            this.front = newNode;
+            this.back = newNode;
+        } else if (this.back != undefined) {
+            this.back.next = newNode;
+            this.back = newNode;
+        }
+        this.length++;
+    }
     deque(): T | undefined {
+        const value = this.front?.value;
 
-}
+        if (this.front != undefined) {
+            if (this.front.next != undefined) {
+                this.front = this.front.next;
+            } else {
+                this.front = undefined;
+                this.back = undefined;
+            }
+            this.length--;
+        }
+        return value;
+    }
     peek(): T | undefined {
-
-}
+        return this.front?.value;
+    }
 }
