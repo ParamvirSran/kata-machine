3c8b4a8edd7ae443472e0c7004f885dbc65465fe
Paramvir Sran <paramvirsran@gmail.com>
2024-06-11 02:10:47 -0400

    implemented stack data structure

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,40 @@
+class Node<T> {
+    value: T;
+    next: Node<T> | undefined;
+
+    constructor(value: T) {
+        this.value = value;
+        this.next = undefined;
+    }
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private top: Node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.top = undefined;
     }
-
     push(item: T): void {
-
-}
+        const newNode = new Node(item);
+        if (this.top == undefined) {
+            this.top = newNode;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length++;
+    }
     pop(): T | undefined {
-
-}
+        const value = this.top?.value;
+        if (this.top != undefined) {
+            this.top = this.top.next;
+            this.length--;
+        }
+        return value;
+    }
     peek(): T | undefined {
-
-}
+        return this.top?.value;
+    }
 }
