2b1ea3f9a8817288ba940fbd3533cb2ba647d0f3
Paramvir Sran <paramvirsran@gmail.com>
2024-06-14 16:09:21 -0400

    wrote code for araylist implementation and passed testing

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,75 @@
 export default class ArrayList<T> {
     public length: number;
+    private arr: Array<T>;
 
-    
-
-    constructor() {
+    constructor(capacity: number) {
+        this.length = 0;
+        this.arr = new Array<T>(capacity);
     }
 
     prepend(item: T): void {
+        if (this.arr.length <= this.length) {
+            this.expand();
+        }
+        for (let i = 0; i < this.length; i++) {
+            this.arr[this.length - i] = this.arr[this.length - i - 1];
+        }
+        this.arr[0] = item;
+        this.length++;
+    }
 
-}
+    expand(): void {
+        let temp = this.arr;
+        this.arr = new Array<T>(this.arr.length * 2);
+        for (let i = 0; i < this.length; i++) {
+            this.arr[i] = temp[i];
+        }
+    }
+
     insertAt(item: T, idx: number): void {
+        if (idx < 0 || idx > this.length) { return; }
+        if (idx === 0) { this.prepend(item); return; }
 
-}
+        if (this.arr.length <= this.length) {
+            this.expand();
+        }
+        for (let i = 0; i < this.length - idx; i++) {
+            this.arr[this.length - i] = this.arr[this.length - i - 1];
+        }
+        this.arr[idx] = item;
+        this.length++;
+    }
     append(item: T): void {
-
-}
+        if (this.arr.length <= this.length) {
+            this.expand();
+        }
+        this.arr[this.length] = item;
+        this.length++;
+    }
     remove(item: T): T | undefined {
+        if (this.length === 0) { return undefined; }
+        let value = undefined;
 
-}
+        for (let i = 0; i < this.length; i++) {
+            if (item === this.arr[i]) {
+                value = this.removeAt(i);
+                return value;
+            }
+        }
+        return value;
+    }
     get(idx: number): T | undefined {
-
-}
+        if (idx < 0 || idx >= this.length) { return undefined; }
+        return this.arr[idx];
+    }
     removeAt(idx: number): T | undefined {
+        if (idx < 0 || idx >= this.length) { return undefined; }
+        const value = this.arr[idx];
 
-}
+        for (let i = idx; i < this.length; i++) {
+            this.arr[i] = this.arr[i + 1];
+        }
+        this.length--;
+        return value;
+    }
 }
