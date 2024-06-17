a8e6173f0902f20b4f511684356d5b84b84ebc63
Paramvir Sran <paramvirsran@gmail.com>
2024-06-17 18:55:39 -0400

    successful implementation of a ringbuffer or a circular array

--- src/day1/RingBuffer.ts
+++ src/day1/RingBuffer.ts
@@ -1,18 +1,36 @@
 export default class RingBuffer<T> {
     public length: number;
-
-    
+    private arr: Array<T>;
+    private head: number;
+    private tail: number;
 
     constructor() {
+        this.length = 0;
+        this.arr = new Array<T>(5);
+        this.head = this.tail = 0;
     }
 
     push(item: T): void {
-
-}
+        if ((this.head % this.arr.length) === (this.tail % this.arr.length)) {
+            let temp = new Array<T>(this.arr.length * 2);
+            for (let i = 0; i < this.length; i++) {
+                temp[i] = this.arr[(this.head + i) % this.arr.length];
+            }
+            this.arr = temp;
+        }
+        this.arr[this.tail % this.arr.length] = item;
+        this.length++;
+        this.tail++;
+    }
     get(idx: number): T | undefined {
-
-}
+        const value = this.arr[(this.head + idx) % this.arr.length];
+        return value;
+    }
     pop(): T | undefined {
-
-}
+        if (this.length === 0) { return undefined; }
+        const value = this.arr[(this.head) % this.arr.length];
+        this.length--;
+        this.head++;
+        return value;
+    }
 }
