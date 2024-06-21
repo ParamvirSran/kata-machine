3c60f9a5107a2e2be7753496a5c17512a095330e
Paramvir Sran <paramvirsran@gmail.com>
2024-06-21 17:29:53 -0400

    finished implementing BFS on binary tree

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,17 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    return walk(head, needle);
+}
 
+function walk(root: BinaryNode<number> | null, needle: number): boolean {
+    if (!root) {
+        return false;
+    } else if (root.value === needle) {
+        return true;
+    } else if (walk(root.left, needle)) {
+        return true;
+    } else if (walk(root.right, needle)) {
+        return true;
+    } else {
+        return false;
+    }
 }
