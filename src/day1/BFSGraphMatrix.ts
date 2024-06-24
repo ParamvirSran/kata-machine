95bca86615010e716a2bb544cd8020391f2f8f83
Paramvir Sran <paramvirsran@gmail.com>
2024-06-24 01:51:29 -0400

    finished BFS on a adjacency matrix code

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,33 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const visited: boolean[] = new Array(graph.length).fill(false);
+    const q: number[] = [];
+    const path: number[] = [];
+    const parent: (number | null)[] = new Array(graph.length).fill(null);
 
+    q.push(source);
+    visited[source] = true;
+
+    while (q.length > 0) {
+        const curr = q.shift();
+        if (curr === undefined) {
+            continue;
+        }
+        if (curr === needle) {
+            let node: number | null = curr;
+            while (node !== null) {
+                path.push(node);
+                node = parent[node];
+            }
+            return path.reverse();
+        }
+
+        for (let i = 0; i < graph[curr].length; i++) {
+            if (graph[curr][i] > 0 && !visited[i]) {
+                visited[i] = true;
+                parent[i] = curr;
+                q.push(i);
+            }
+        }
+    }
+    return null;
 }
