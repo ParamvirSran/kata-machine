7b8e9f615914087db29653b4c9bd78ae9fa2290e
Paramvir Sran <paramvirsran@gmail.com>
2024-06-26 19:45:42 -0400

    reimplementing dijkstra algorithm

--- src/day1/DijkstraList.ts
+++ src/day1/DijkstraList.ts
@@ -1,54 +1,58 @@
-export default function dijkstra_list(source: number, sink: number, arr: WeightedAdjacencyList): number[] {
-    const distances: number[] = new Array(arr.length).fill(Infinity);
-    const parent: number[] = new Array(arr.length).fill(null);
-    const visited: boolean[] = new Array(arr.length).fill(false);
-    const queue: number[] = [source];
-    distances[source] = 0;
-
-    while (queue.length > 0) {
-        const curr = queue.splice(find_min(queue, distances), 1)[0];
-
-        if (curr === sink) {
-            break;
-        }
-        visited[curr] = true;
-
-        // Relax adjacent edges of current node
-        for (const edge of arr[curr]) {
-            if (!visited[edge.to]) {
-                relax(parent, distances, edge, curr);
-                if (!queue.includes(edge.to)) {
-                    queue.push(edge.to);
-                }
-            }
+function has_unvisited(visited: boolean[], distances: number[]): boolean {
+    for (let i = 0; i < visited.length; i++) {
+        if (!visited[i] && distances[i] < Infinity) {
+            return true;
         }
     }
-    // Reconstruct path from sink to source
-    const path: number[] = [];
-    for (let curr: number | null = sink; curr !== null; curr = parent[curr]) {
-        path.push(curr);
-    }
-    return path.reverse();
+    return false;
 }
 
-// Get minimum distance node to visit next
-function find_min(queue: number[], distances: number[]): number {
+function minimum_unvisited_index(visited: boolean[], distances: number[]): number {
     let smallest_distance = Infinity;
-    let currentIndex = 0;
+    let currentIndex = -1;
 
-    for (let i = 0; i < queue.length; i++) {
-        if (distances[queue[i]] < smallest_distance) {
-            smallest_distance = distances[queue[i]];
+    for (let i = 0; i < visited.length; i++) {
+        if (!visited[i] && distances[i] < smallest_distance) {
+            smallest_distance = distances[i];
             currentIndex = i;
         }
     }
     return currentIndex;
 }
 
-// Update distance and parent of neighbor from the current node if better route found
-function relax(parent: (number | null)[], distances: number[], edge: GraphEdge, curr: number) {
-    if (distances[edge.to] > distances[curr] + edge.weight) {
-        distances[edge.to] = distances[curr] + edge.weight;
+function relax(parent: number[], distances: number[], edge: GraphEdge, curr: number) {
+    let new_path = distances[curr] + edge.weight;
+
+    if (distances[edge.to] > new_path) {
         parent[edge.to] = curr;
+        distances[edge.to] = new_path;
     }
+}
+
+export default function dijkstra_list(source: number, sink: number, arr: WeightedAdjacencyList): number[] {
+    const visited: boolean[] = new Array(arr.length).fill(false);
+    const distances: number[] = new Array(arr.length).fill(Infinity);
+    const parent: number[] = new Array(arr.length).fill(-1);
+    distances[source] = 0;
+
+    while (has_unvisited(visited, distances)) {
+        const curr = minimum_unvisited_index(visited, distances);
+
+        if (curr === sink) {
+            break;
+        }
+        visited[curr] = true;
+
+        for (const edge of arr[curr]) {
+            if (!visited[edge.to]) {
+                relax(parent, distances, edge, curr);
+            }
+        }
+    }
+    const path = [];
+
+    for (let curr = sink; curr !== -1; curr = parent[curr]) {
+        path.push(curr);
+    }
+    return path.reverse();
 }
